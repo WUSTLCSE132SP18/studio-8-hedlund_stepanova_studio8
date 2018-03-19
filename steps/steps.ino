284c63068373b0f3c3c9345b2b647c12a2f066cd
shell <shell@DESKTOP-PCNR08D.dhcp.wustl.edu>
2018-03-19 17:08:57 -0500

    steps updated

--- steps/steps.ino
+++ steps/steps.ino
@@ -1,1 +1,26 @@
+#include <Wire.h>
+#include <SparkFun_MMA8452Q.h>
 
+MMA8452Q accel;
+
+void setup(){
+  Serial.begin(9600);
+  accel.init();
+}
+
+void loop(){
+  if (accel.available()){
+    accel.read();
+    printCalculatedAccels();
+  }
+}
+
+
+void printCalculatedAccels()
+{ 
+  Serial.print(accel.cx, 3);
+  Serial.print(",");
+  Serial.print(accel.cy, 3);
+  Serial.print(",");
+  Serial.println(accel.cz, 3);
+}
